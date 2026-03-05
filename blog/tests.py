a5037dc3880a4ba77259991acfb2a12926f51939
Adam <25955@edu.amw.gdynia.pl>
2026-03-05 09:52:18 +0100

    test

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Post
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class PostModelTest(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(username="testuser")
+    
+    def test_post_creation(self):
+        post = Post.objects.create(
+            title="Test Post",
+            content="Test Content",
+            author=self.user
+        )
+
+        self.assertEqual(post.title, "Test Post")
+        self.assertEqual(str(post), "Test Post")
