0864454862b8387d892e922cf83240ef706e18f0
Adam <25955@edu.amw.gdynia.pl>
2026-03-05 09:32:26 +0100

    Implement basic views and templates for blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView, DetailView
+from .models import Post
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/postList.html'
+    context_object_name = 'posts'
+    ordering = ['-created_at']
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/postDetail.html'
+    context_object_name = 'post'
