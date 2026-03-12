98edf4221fbfdfd149452a539ac9b6236bfbcf54
Adam <25955@edu.amw.gdynia.pl>
2026-03-12 08:48:19 +0100

    Add basic JSON API endpoint for posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PostListView, PostDetailView
+from .views import PostListView, PostDetailView, posts_api
 
 urlpatterns = [
     path('', PostListView.as_view(), name='postList'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='postDetail'),
+    path('api/posts/', posts_api, name='posts_api'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 from django.views.generic import ListView, DetailView
 from .models import Post
 
@@ -15,3 +15,17 @@ class PostDetailView(DetailView):
     model = Post
     template_name = 'blog/postDetail.html'
     context_object_name = 'post'
+
+def posts_api(request):
+    posts = Post.objects.all()
+    data = []
+    for post in posts:
+        data.append({
+            "id": post.id,
+            "title": post.title,
+            "content": post.content,
+            "author": post.author.username,
+            "created_at": post.created_at,
+            "published_at": post.published_at
+        })
+    return JsonResponse(data, safe=False)
