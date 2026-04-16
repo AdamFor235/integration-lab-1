3d8649521ae9f4ba12edb0efd47a6947399e4ab7
Adam <25955@edu.amw.gdynia.pl>
2026-04-16 12:21:48 +0200

    Add unit tests for the application

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+from .models import Post
+from django.contrib.auth.models import User
+
+class PostModelTest(TestCase):
+    def test_create_post(self):
+        user = User.objects.create_user(
+            username="testuser",
+            password="testpass"
+        )
+        post = Post.objects.create(
+            title="Test title",
+            content="Test content",
+            author=user
+        )
+        self.assertEqual(post.title, "Test title")
+        self.assertEqual(post.content, "Test content")
+
+
+class PostViewTest(TestCase):
+    def test_post_list_view(self):
+        response = self.client.get(reverse('postList'))
+        self.assertEqual(response.status_code, 200)
